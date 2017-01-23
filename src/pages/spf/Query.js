322ecc23ceb33ffcf3310db380537604d02e7907
Cole Furfaro-Strode <colestrode@gmail.com>
2017-01-23 16:50:22 -0500

    FAD-4221 adds basic handler for validation and transitioning to spf results

--- src/pages/spf/Query.js
+++ src/pages/spf/Query.js
@@ -1,17 +1,32 @@
 /* eslint-disable no-console */
 
 import React, { Component } from 'react';
+import classNames from 'classnames';
 import { INTRO_TEXT } from './constants';
-import { LinkButton } from 'components/button/Button';
+import { ActionButton } from 'components/button/Button';
 
 
 export default class Query extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      domain: ''
+      domain: '',
+      domainError: false
     };
   }
+
+  validateDomain() {
+    return this.state.domain && this.state.domain.test(/^https?:\/\/[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}$/);
+  }
+
+  goToDomainResults(domain) {
+    this.setState({domainError: this.validateDomain(domain)});
+
+    if (!this.state.domainError) {
+      // go to `/spf/results/${this.state.domain}`
+    }
+  }
+
 
   renderPanel() {
     return (
@@ -19,21 +34,25 @@ export default class Query extends Component {
         <div className='panel__body'>
           <h4>Inspect a Domain</h4>
           <p className='paddingBottom--md text--muted'>Enter a domain to inspect, e.g., sparkpost.com.</p>
-          <div className='flex'>
-
-            <div className='col-xs-12 h-hide'>
-              {/* i want this easter egg, but it's served over http, so might cause some mixed content warnings */}
-              <img alt='spferlock holmes' title='Spferlock Holmes' src='http://cliparts.co/cliparts/5TR/Kj5/5TRKj55qc.jpg'/>
+          <form>
+            <div className="flex h-hide">
+              <div className='col-xs-12'>
+                {/* i want this easter egg, but it's served over http, so might cause some mixed content warnings */}
+                <img alt='spferlock holmes' title='Spferlock Holmes' src='http://cliparts.co/cliparts/5TR/Kj5/5TRKj55qc.jpg'/>
+              </div>
             </div>
 
-            <div className='col-xs-8'>
-              <input className='input__text input--full' type='text' onChange={(e) => this.setState({domain: e.target.value}) } placeholder='mydomain.com' />
-            </div>
+            <div className="flex">
+              <div className={classNames('col-xs-8', {'has-error': this.state.domainError })}>
+                <input className='input__text input--full' type='text' onChange={(e) => this.setState({domain: e.target.value}) } placeholder='mydomain.com' />
+                <span className="input__error">Please</span>
+              </div>
 
-            <div className='col-xs-4'>
-              <LinkButton type='orange' fullWidth={true} to={`/spf/results/${this.state.domain}`}>View Results</LinkButton>
+              <div className='col-xs-4'>
+                <ActionButton type='orange' fullWidth={true} action={this.goToDomainResults()}>View Results</ActionButton>
+              </div>
             </div>
-          </div>
+          </form>
         </div>
       </div>
     );
