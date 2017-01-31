4604782d5d230e4d65831cb72651fe5b8f63db25
Andrew Winder <andrew.winder@sparkpost.com>
2017-01-31 15:15:22 -0500

    FAD-4222 Styling up the history row

--- src/components/spf/ResultListRow.js
+++ src/components/spf/ResultListRow.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
+import { LinkButton } from 'components/button/Button';
 import Icon from 'components/Icon';
 
 class ResultListRow extends Component {
@@ -7,18 +7,22 @@ class ResultListRow extends Component {
     const { domain, timestamp } = this.props;
 
     return (
-      <div>
-        <div>
-          {this.renderStatus()}
-        </div>
-        <div>
-          {domain}
-          <span>Inspected on {timestamp}</span>
-        </div>
-        <div>
-          <Link to={`/spf/results/${domain}`}>
-            Refresh
-          </Link>
+      <div className="panel panel--accent text--left">
+        <div className="panel__body">
+          <div style={{ justifyContent: 'center', alignItems: 'center' }} className="flex">
+            <div style={{ fontSize: '32px' }} className="col-xs-2 col-md-2 col-lg-2">
+              {this.renderStatus()}
+            </div>
+            <div className="col-xs-10 col-md-8 col-lg-8">
+              <h4>{domain}</h4>
+              <div className="paddingBottom--sm text--muted">
+                Inspected on {timestamp}
+              </div>
+            </div>
+            <div className="col-xs-12 col-md-2 col-lg-2">
+              <LinkButton type='orange' to={`/spf/results/${domain}`}>View</LinkButton>
+            </div>
+          </div>
         </div>
       </div>
   );
@@ -29,13 +33,13 @@ class ResultListRow extends Component {
     let iconCls = '';
     switch (status) {
       case 'valid':
-        iconCls = 'check-status';
+        iconCls = 'check';
         break;
       case 'warning':
-        iconCls = 'warning';
+        iconCls = 'exclamation';
         break;
       case 'error':
-        iconCls = 'exclamation-circle';
+        iconCls = 'exclamation-triangle';
         break;
     }
     return (<Icon name={iconCls} />);
