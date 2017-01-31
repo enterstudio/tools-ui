74d065cb67eba81a1b634d6eae22b19c0e0c25d7
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2017-01-31 14:09:02 -0500

    FAD-4332 adding in the basics of redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
+import reducers from './reducers';
 import routes from './routes';
 import './styles/tools.scss';
 
+const store = createStore(reducers);
+
 ReactDOM.render((
-  <Router history={browserHistory} routes={routes} />
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>
 ), document.getElementById('root'));
