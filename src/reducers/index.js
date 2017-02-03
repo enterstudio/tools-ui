21634b517599fcda3e9881c13bfde6a685857a92
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2017-02-03 13:34:19 -0500

    FAD-4332 auth reducer for loggedin and token state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,16 +1,10 @@
 import { combineReducers } from 'redux';
-<<<<<<< HEAD
 import authReducer from './auth';
-=======
->>>>>>> FAD-4332 refactored the redux flow into 2 reducers and a tree helper
 import spfTreeReducer from './spfTree';
 import spfInspectReducer from './spfInspect';
 
 const reducers = {
-<<<<<<< HEAD
   auth: authReducer,
-=======
->>>>>>> FAD-4332 refactored the redux flow into 2 reducers and a tree helper
   spfInspect: combineReducers({
     tree: spfTreeReducer,
     details: spfInspectReducer
