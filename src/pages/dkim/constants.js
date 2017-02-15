85f5f77427fbd3f645e003e1fdddd7d21251f8dc
Jon Ambas <jon@jonambas.com>
2017-02-15 17:00:25 -0500

    FAD-4434 Add react-helmet & Setup usage example

--- src/pages/dkim/constants.js
+++ src/pages/dkim/constants.js
@@ -2,3 +2,14 @@ export const INTRO_TEXT = 'Verifying DKIM is one step that can help your email g
 
 export const LIST_ERROR_MESSAGE = 'Sorry, we\'re having trouble finding your messages.';
 export const DETAIL_ERROR_MESSAGE = 'Sorry, we\'re having trouble finding your message details.';
+
+// https://github.com/nfl/react-helmet
+export const HOMEPAGE_HELMET = {
+  title: 'DKIM Validator',
+  meta: [
+    {name: 'description', content: 'DKIM Validator Description'},
+    {property: 'og:type', content: 'website'},
+    {property: 'og:url', content: 'https://tools.sparkpost.com/dkim'}
+    // {property: 'og:image', content: ''}
+  ]
+};
